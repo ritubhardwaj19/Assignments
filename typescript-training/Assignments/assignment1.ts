fc7594a0b47421cb37d63d10c4dfbd3845a43117
Ritu Bhardwaj <ritubhard19@gmail.com>
2026-05-27 00:06:15 +0530

    assignment1

---
+++ typescript-training/Assignments/assignment1.ts
@@ -0,0 +1,119 @@
+// 1 Temperature of a city in degrees Celsius: 25.5
+
+
+let temp : number = 25.5
+console.log( `the temperature of a city ${temp}`);
+
+// 2. Whether a customer has placed an order: true or false
+
+let customer : boolean = true;
+
+console.log(customer);
+
+// 3. Person's phone number: "123-456-7890"
+
+let phnumber : number = "123-456-7890";
+
+console.log(phnumber);
+
+//4. Amount of money in a customer's bank account: 1000.50
+
+let bankacc : number = 1000.50;
+ 
+console.log(bankacc);
+
+//5. Person's email address: "john.doe@example.com"
+
+let email : string = "john.doe@example.com";
+
+console.log(`email of an employee ${email}`);
+
+//6
+
+let latitude: number = 37.7749;
+let longitude: number = -122.4194;
+
+console.log(latitude);
+console.log(longitude);
+
+//7 Person's marital status: true or false
+ 
+let maritalstatus : string = false;
+
+console.log(maritalstatus);
+
+//8 Person's occupation: "Software Engineer"
+
+let occupation : string = "Software Engineer";
+console.log(occupation);
+
+//9 Person's favourite colour: "Blue"
+
+let favouriteColour: string = "Blue";
+
+console.log(favouriteColour);
+
+// 10.Current year: 2023
+
+let currentyear : number = 2023;
+
+console.log(currentyear);
+
+//11.Number of followers on a social media platform: 1,000,000
+
+let followers : number = "1,000,000";
+
+console.log(followers);
+
+//12 
+let movieRating: number = 7.5;
+
+console.log(movieRating);
+
+
+// 13.Person's blood type: 'A'
+
+const bloodtype : string = "A";
+
+console.log(bloodtype);
+
+//14.Title of a book: "To Kill a Mockingbird"
+
+let titleofbook : string = "To Kill a Mockingbird";
+
+console.log(titleofbook);
+
+//15.Number of employees in a company: 500
+
+let noofemployee : number = 500;
+console.log(noofemployee);
+
+//// 16. Time of an event
+
+let eventTime: string = "2:30 PM";
+
+console.log(eventTime);
+
+// 17. Name of a country
+
+let countryName: string = "United States";
+
+console.log(countryName);
+
+// 18. Person's eye color
+
+let eyeColor: string = "Brown";
+
+console.log(eyeColor);
+
+// 19. Person's birthplace
+
+let birthplace: string = "New York City";
+
+console.log(birthplace);
+
+// 20. Distance between two cities
+
+let distance: number = 200.5;
+
+console.log(distance);
---
+++ typescript-training/language-basics/data-types.ts
@@ -0,0 +1,33 @@
+
+//string 
+let firstname : string = "ritu";
+let lastname : string = "bhardwaj";
+console.log(`employee firstname is ${firstname} and lastname is ${lastname}`);
+
+
+// boolean 
+
+let doyouhavevisa : boolean = "true";
+console.log(doyouhavevisa);
+
+//undefined
+
+let empage : undefined 
+
+console.log(empage);
+
+//null
+
+let customerage : null = "null";
+
+console.log(customerage);
+
+
+//union
+
+let empadd : string | number;
+empadd = "hyd";
+console.log(empadd);
+empadd = 50049;
+
+console.log(empadd);
